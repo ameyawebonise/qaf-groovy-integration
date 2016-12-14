a4fc3a0392120ad2ab5f2a61f76a8b8f4bc99cab
Ameya <ameya.kulkarni@weboniselab.com>
2016-12-14 14:58:11 +0530

    Made the code compile and call groovy from a java class

---
+++ src/main/groovy/com/qmetry/qaf/automation/ConsumptionService.groovy
@@ -0,0 +1,14 @@
+package com.qmetry.qaf.automation
+
+import groovy.transform.CompileStatic
+
+/**
+ * Created by Webonise on 13/12/16.
+ */
+@CompileStatic
+public class ConsumptionService {
+
+    public def sayHello(){
+        println("hello all!")
+    }
+}
---
+++ src/main/java/com/qmetry/qaf/automation/Entry.java
@@ -0,0 +1,15 @@
+package com.qmetry.qaf.automation;
+
+
+
+/**
+ * Created by Webonise on 14/12/16.
+ */
+
+public class Entry {
+
+    public static void main(String[] args){
+        ConsumptionService c = new ConsumptionService();
+        c.sayHello();
+    }
+}

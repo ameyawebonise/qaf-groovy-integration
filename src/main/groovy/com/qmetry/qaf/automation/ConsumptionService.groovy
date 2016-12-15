cdd2eab79ae666a99281c81bc460d0e8298bc69d
Harshali <harshali.nemade@weboniselab.com>
2016-12-15 15:27:23 +0530

    Changes

--- src/main/groovy/com/qmetry/qaf/automation/ConsumptionService.groovy
+++ src/main/groovy/com/qmetry/qaf/automation/ConsumptionService.groovy
@@ -1,14 +1,60 @@
 package com.qmetry.qaf.automation
 
+import geb.Browser
+import geb.binding.BindingUpdater
 import groovy.transform.CompileStatic
+import geb.*
+
+import static cucumber.api.groovy.EN.*
+import static cucumber.api.groovy.Hooks.*
+import java.util.concurrent.TimeUnit
+
+import  com.qmetry.qaf.automation.NewsPage
+import com.qmetry.qaf.automation.ui.UiDriverFactory.Browsers
+import  com.qmetry.qaf.automation.HomePage
 
 /**
  * Created by Webonise on 13/12/16.
  */
-@CompileStatic
-public class ConsumptionService {
-
-    public def sayHello(){
-        println("hello all!")
-    }
-}
+//@CompileStatic
+//public class ConsumptionService {
+def configbinding
+	public def sayHello(){
+		 println("hello all groovy user!")
+	 }
+	 
+	//BEfore and after are cucumber hooks
+	 def executebefore() {
+	  Before() {
+		  println("In before!")
+		  bindingUpdater = new BindingUpdater(binding, new Browser())
+		  bindingUpdater.initialize()
+		  browser.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+		  browser.driver.manage().window().maximize()
+	  }
+     }
+	// def executeafter()
+	// {
+		   After(){ bindingUpdater.remove() }
+	 //}
+  
+	  /*
+	  def userhome (){
+		  
+		  
+		  helpLink(to: HomePage)
+		 
+		  
+	  //	 { -> to HomePage }
+	   
+		 }*/
+  /*
+  
+  When(~/^I go to my bag$/) {
+	  ->
+	  page.selectLatestNews()
+	  at NewsPage
+	  page.check_item()
+  }
+	  
+  Then(~/^user successfully able to view my bag$/) { -> at HomePage} */
--- src/main/java/com/qmetry/qaf/automation/Entry.java
+++ src/main/java/com/qmetry/qaf/automation/Entry.java
@@ -1,6 +1,14 @@
 package com.qmetry.qaf.automation;
 
+import com.qmetry.qaf.automation.step.QAFTestStep;
 
+import groovy.lang.Binding;
+//import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.util.GroovyScriptEngine;
+
+import static com.qmetry.qaf.automation.step.CommonStep.click;
+import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
 
 /**
  * Created by Webonise on 14/12/16.
@@ -8,8 +16,61 @@ package com.qmetry.qaf.automation;
 
 public class Entry {
 
+	/*
     public static void main(String[] args){
         ConsumptionService c = new ConsumptionService();
         c.sayHello();
     }
+    */
+    
+     /*
+	 String[] paths = {"C:\\Users\\webonise\\git\\qaf-groovy\\qaf-groovy-integration\\src\\main\\groovy\\com\\qmetry\\qaf\\automation"};
+		  GroovyScriptEngine gse = new GroovyScriptEngine(paths);
+		  Binding binding = new Binding();
+		  Object[] path = {"C:\\Users\\webonise\\git\\qaf-groovy\\qaf-groovy-integration"};
+		  binding.setVariable("args",path);
+		  
+		  gse.run("Songs.groovy", binding); */
+	
+	
+	/**
+	 * @param searchTerm
+	 *            : search term to be searched
+	 */
+	
+	@QAFTestStep(description = "I am on the home page")
+	public static void searchFor() {
+		ConsumptionService c = new ConsumptionService();
+      // c.executebefore();
+       TestingPage tp = new TestingPage();
+       tp.clickhome();
+	//	 c.executebefore();
+	 //    c.userhome();
+	  //   c.executeafter();
+	
+	}
+	
+	
+	@QAFTestStep(description="I go to my bag")
+	public void sendkeysTo(){
+	
+		System.out.println("In when--entryjava file  ");
+		ConsumptionService c = new ConsumptionService();
+	    //   c.executebefore();
+	       TestingPage tp = new TestingPage();
+	 
+	       tp.clickhome();
+	//	ConsumptionService c = new ConsumptionService();
+		//c.goHomepage();
+	//	c.userhome();
+	
+	}
+	
+	@QAFTestStep(description="user successfully able to view my bag")
+	public void thencheck(){
+	
+		System.out.println("then---entry java file ");
+		
+	
+	}
 }
